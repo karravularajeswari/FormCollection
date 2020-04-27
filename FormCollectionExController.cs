286bdd5921f5984c20e76370c750aa600baf5d30
karravularajeswari <43335512+karravularajeswari@users.noreply.github.com>
2020-04-27 14:43:30 +0530

    Add files via upload

---
+++ FormCollectionExController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCPractice.Controllers
+{
+    public class FormCollectionExController : Controller
+    {
+        // GET: FormCollectionEx
+        public ViewResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult Index(FormCollection frm)
+        {
+            ViewBag.name = frm["txtname"];
+            ViewBag.price = frm["txtprice"];
+            return View();
+        }
+    }
+}
